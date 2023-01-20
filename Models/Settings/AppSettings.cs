c3aa3f4ac4bfab0a334067a5df83696175e4434f
OakeseskAo <oakes.a@live.com>
2023-01-19 23:58:18 -0500

    More init
    More init

---
+++ Models/Settings/AppSettings.cs
@@ -0,0 +1,6 @@
+﻿namespace ReelRoster.Models.Settings
+{
+    public class AppSettings
+    {
+        public ReelRosterSettings ReelRosterSettings { get; set; }
+        public TMDBSettings TMDBSettings { get; set; }
