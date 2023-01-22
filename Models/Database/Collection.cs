be05a311f9296eef41e4293ad1b1f19726d43985
OakeseskAo <oakes.a@live.com>
2023-01-21 21:10:45 -0500

    Create Collection.cs
    Created collection model

---
+++ Models/Database/Collection.cs
@@ -0,0 +1,10 @@
+﻿namespace ReelRoster.Models.Database
+{
+    public class Collection
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
