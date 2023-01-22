235263887df302007910899505a1f2f6c3bf862f
OakeseskAo <oakes.a@live.com>
2023-01-21 21:33:12 -0500

    Create LandingPageVM.cs
    Added Landing page custom view models

---
+++ Models/ViewModels/LandingPageVM.cs
@@ -0,0 +1,17 @@
+﻿using ReelRoster.Models.Database;
+using ReelRoster.Models.TMDB;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace ReelRoster.Models.ViewModels
+{
+    public class LandingPageVM
+    {
+        public List<Collection> CustomCollections { get; set; }
+        public MovieSearch NowPlaying { get; set; }
+        public MovieSearch Popular { get; set; }
+        public MovieSearch TopRated { get; set; }
+        public MovieSearch Upcoming { get; set; }
+
+    }
+}
