f4a89cc37839934dde525042c3c86e921ea16e42
OakeseskAo <oakes.a@live.com>
2023-01-22 17:19:24 -0500

    Create TMDBMappingService.cs
    Added MapDetailAsync function

---
+++ Services/TMDBMappingService.cs
@@ -0,0 +1,153 @@
+﻿using ReelRoster.Enums;
+using ReelRoster.Models.Database;
+using ReelRoster.Models.Settings;
+using ReelRoster.Models.TMDB;
+using ReelRoster.Services.Interfaces;
+using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace ReelRoster.Services
+{
+    public class TMDBMappingService : IDataMappingSerivce
+    {
+        private AppSettings _appSettings;
+        private readonly IImageService _imageService;
+
+        public TMDBMappingService(AppSettings appSettings, IImageService imageService)
+        {
+            _appSettings = appSettings;
+            _imageService = imageService;
+        }
+
+
+        public ActorDetail MapActorDetail(ActorDetail actor)
+        {
+
+        }
+
+        public async Task<Movie> MapMovieDetailAsync(MovieDetail movie)
+        {
+            Movie newMovie = null;
+
+            try
+            {
+                newMovie = new Movie()
+                {
+                    MovieId = movie.id,
+                    Title = movie.title,
+                    TagLine = movie.tagline,
+                    Overview = movie.overview,
+                    RunTime = movie.runtime,
+                    VoteAverage = movie.vote_average,
+                    ReleaseDate = DateTime.Parse(movie.release_date),
+                    TrailerUrl = BuildTrailerPath(movie.videos),
+                    Backdrop = await EncodeBackdropImageAsync(movie.backdrop_path),
+                    BackdropType = BuildImageType(movie.backdrop_path),
+                    Poster = await EncodePosterImageAsync(movie.poster_path),
+                    PosterType = BuildImageType(movie.poster_path),
+                    Rating = GetRating(movie.release_dates)
+
+                };
+
+                var castMembers = movie.credits.cast.OrderByDescending(c => c.popularity)
+                                                    .GroupBy(c => c.cast_id)
+                                                    .Select(g => g.FirstOrDefault())
+                                                    .Take(20)
+                                                    .ToList();
+
+                castMembers.ForEach(member =>
+                {
+                    newMovie.Cast.Add(new MovieCast()
+                    {
+                        CastId = member.id,
+                        Department = member.known_for_department,
+                        Name = member.name,
+                        Character = member.character,
+                        ImageUrl = BuildCastImage(member.profile_path),
+                    });
+                });
+
+
+                var crewMembers = movie.credits.crew.OrderByDescending(c => c.popularity)
+                                                    .GroupBy(c => c.id)
+                                                    .Select(g => g.First())
+                                                    .Take(20)
+                                                    .ToList();
+
+                crewMembers.ForEach(member =>
+                {
+                    newMovie.Crew.Add(new MovieCrew()
+                    {
+                        CrewId = member.id,
+                        Department = member.department,
+                        Name = member.name,
+                        Job = member.job,
+                        ImageUrl = BuildCastImage(member.profile_path),
+                    });
+                });
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception in MapMovieDetailAsync : {ex.Message}");
+            }
+
+            return newMovie;
+
+        }
+
+        private string BuildCastImage(string profilePath)
+        {
+            if (string.IsNullOrEmpty(profilePath))
+                return _appSettings.ReelRosterSettings.DefaultCastImage;
+
+            return $"{_appSettings.TMDBSettings.BaseImagePath}/{_appSettings.ReelRosterSettings.DefualtPosterSize}/{profilePath}";
+        }
+
+        private MovieRating GetRating(Release_Dates dates)
+        {
+            var movieRating = MovieRating.NR;
+            var certification = dates.results.FirstOrDefault(r => r.iso_3166_1 == "US");
+            if (certification is not null)
+            {
+                var apiRating = certification.release_dates.FirstOrDefault(c => c.certification != "")?.certification.Replace("-", "");
+                if (!string.IsNullOrEmpty(apiRating))
+                {
+                    movieRating = (MovieRating)Enum.Parse(typeof(MovieRating), apiRating, true);
+                }
+            }
+            return movieRating;
+        }
+
+        private async Task<byte[]> EncodePosterImageAsync(string path)
+        {
+            var posterPath = $"{_appSettings.TMDBSettings.BaseImagePath}/{_appSettings.ReelRosterSettings.DefualtPosterSize}/{path}";
+            return await _imageService.EncodeImageURLAsync(posterPath);
+        }
+
+        private string BuildImageType(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+                return path;
+
+            return $"image/{Path.GetExtension(path).TrimStart('.')}";
+        }
+
+        private async Task<byte[]> EncodeBackdropImageAsync(string path)
+        {
+            var backdropPath = $"{_appSettings.TMDBSettings.BaseImagePath}/{_appSettings.ReelRosterSettings.DefaultBackdropSize}/{path}";
+            return await _imageService.EncodeImageURLAsync(backdropPath);
+        }
+
+        private string BuildTrailerPath(Videos videos)
+        {
+            var videoKey = videos.results.FirstOrDefault(r => r.type.ToLower().Trim() == "trailer" && r.key != "")?.key;
+            return string.IsNullOrEmpty(videoKey) ? videoKey : $"{_appSettings.TMDBSettings.BaseYouTubePath} {videoKey}";
+        }
+    }
+}
