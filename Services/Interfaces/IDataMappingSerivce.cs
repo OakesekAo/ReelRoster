c3302d50be9e6a50cd92e53eb803b3598487cb69
OakeseskAo <oakes.a@live.com>
2023-01-21 22:01:59 -0500

    Mapping and Image Interface added
    setting up interfaces

--- Services/Interfaces/IDataMappingSerivce.cs
+++ Services/Interfaces/IDataMappingSerivce.cs
@@ -1,6 +1,13 @@
-﻿namespace ReelRoster.Services.Interfaces
+﻿using ReelRoster.Models.Database;
+using ReelRoster.Models.TMDB;
+using System.Threading.Tasks;
+
+namespace ReelRoster.Services.Interfaces
 {
     public interface IDataMappingSerivce
     {
+        Task<Movie> MapMovieDetailAsync(MovieDetail movie);
+        ActorDetail MapActorDetail(ActorDetail actor);
+
     }
 }
---
+++ Services/Interfaces/IImageService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using System.Threading.Tasks;
+
+namespace ReelRoster.Services.Interfaces
+{
+    public interface IImageService
+    {
+        Task<byte[]> EncodeImageAsync(IFormFile poster);
+        Task<byte[]> EncodeImageURLAsync(string imageURL);
+        string DecodeImage(byte[] poster, string ContentType);
+
+    }
+}
