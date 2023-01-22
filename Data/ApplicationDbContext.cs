b09e68d23708ad17b95f3ed0161737fc1ec3f338
OakeseskAo <oakes.a@live.com>
2023-01-21 22:53:12 -0500

    updated
    Updated ApplicationDbContext and Seed Services

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ReelRoster.Models.Database;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,6 +12,10 @@ namespace ReelRoster.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
         }
+
+
+        public DbSet<Collection> Collection { get; set; }
     }
 }
---
+++ Services/SeedService.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualBasic;
+using ReelRoster.Data;
+using ReelRoster.Models.Settings;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReelRoster.Services
+{
+    public class SeedService
+    {
+        private readonly AppSettings _appSettings;
+        private readonly ApplicationDbContext _dbContext;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityUser> _roleManager;
+
+        public SeedService(IOptions<AppSettings> appSettings, ApplicationDbContext dbContext, UserManager<IdentityUser> userManager, RoleManager<IdentityUser> roleManager)
+        {
+            _appSettings = appSettings.Value;
+            _dbContext = dbContext;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task ManageDataAsync()
+        {
+            await UpdateDatabaseAsync();
+            await SeedRolesAsync();
+            await SeedUserAsync();
+            await SeedCollection();
+        }
+
+        private async Task UpdateDatabaseAsync()
+        {
+            await _dbContext.Database.MigrateAsync();
+        }
+
+        private async Task SeedRolesAsync()
+        {
+            if (_dbContext.Roles.Any()) return;
+            var adminRole = _appSettings.ReelRosterSettings.DefaultCredentials.Role;
+
+            await _roleManager.CreateAsync(new IdentityRole(adminRole));
+        }
+
+        private async Task SeedUserAsync()
+        {
+            if (_userManager.Users.Any()) return;
+
+            var credentials = _appSettings.ReelRosterSettings.DefaultCredentials;
+            var newUser = new IdentityUser()
+            {
+                Email = credentials.Email,
+                UserName = credentials.Email,
+                EmailConfirmed = true
+            };
+
+            await _userManager.CreateAsync(newUser, credentials.Password);
+            await _userManager.CreateAsync(newUser, credentials.Role);
+        }
+
+        private async Task SeedCollection()
+        {
+            if (_dbContext.Collection.Any()) return;
+
+            _dbContext.Add(new Collection()
+            {
+                Name = _appSettings.ReelRosterSettings.DefaultCollection.Name,
+                Description = _appSettings.ReelRosterSettings.DefaultCollection.Description
+            });
+
+            await _dbContext.SaveChangesAsync();
+        }
+
+
+    }
+}
