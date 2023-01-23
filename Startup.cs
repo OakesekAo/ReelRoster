756a576993c8f78ba43374368f0f3d8be5823961
OakeseskAo <oakes.a@live.com>
2023-01-22 20:51:45 -0500

    Update Startup.cs
    Registered image service

--- Startup.cs
+++ Startup.cs
@@ -52,6 +52,7 @@ namespace ReelRoster
             services.AddHttpClient();
             services.AddScoped<IRemoteMovieService, TMDBMovieService>();
             services.AddScoped<IDataMappingSerivce, TMDBMappingService>();
+            services.AddSingleton<IImageService, BasicImageService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
