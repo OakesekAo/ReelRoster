8d7db1382203a5ae2261ee7bdfdda16966e44ded
OakeseskAo <oakes.a@live.com>
2023-01-22 22:54:43 -0500

    Updated DB and created controller
    Updated DB and created controller

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using ReelRoster.Data;
+using ReelRoster.Models.Database;
+using ReelRoster.Models.Settings;
+using ReelRoster.Services.Interfaces;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReelRoster.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppSettings _appSettings;
+        private readonly ApplicationDbContext _context;
+        private readonly IImageService _imageService;
+        private readonly IRemoteMovieService _tmdbMovieService;
+        private readonly IDataMappingSerivce _tmdbMappingService;
+
+        public MoviesController(IOptions<AppSettings> appSettings, ApplicationDbContext context, IImageService imageService, IRemoteMovieService tmdbMovieService, IDataMappingSerivce tmdbMappingService)
+        {
+            _appSettings = appSettings.Value;
+            _context = context;
+            _imageService = imageService;
+            _tmdbMovieService = tmdbMovieService;
+            _tmdbMappingService = tmdbMappingService;
+        }
+
+        public async Task<IActionResult> Import()
+        {
+            var movies = await _context.Movie.ToListAsync();
+            return View(movies);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Import(int id)
+        {
+            // if we already have this, warn user and dont import again
+            if (_context.Movie.Any(m => m.MovieId == id))
+            {
+                var localMovie = await _context.Movie.FirstOrDefaultAsync(m => m.MovieId == id);
+                return RedirectToAction("Details", "Movies", new { id = localMovie.Id, local = true });
+            }
+
+            // get the raw data from the API
+            var movieDetail = await _tmdbMovieService.MovieDetailAsync(id);
+
+            // run the data through the mapping service
+            var movie = await _tmdbMappingService.MapMovieDetailAsync(movieDetail);
+
+            //add the new movie
+
+            _context.Add(movie);
+            await _context.SaveChangesAsync();
+
+            //Assign it to the default "All" Collection
+            await AddToMovieCollection(movie.Id, _appSettings.ReelRosterSettings.DefaultCollection.Name);
+
+            return RedirectToAction("Import");
+
+        }
+
+        private async Task AddToMovieCollection(int movieId, string collectionName)
+        {
+            var collection = await _context.Collection.FirstOrDefaultAsync(c => c.Name == collectionName);
+            _context.Add(
+                new MovieCollection()
+                {
+                    CollectionId = collection.Id,
+                    MovieId = movieId
+                }
+            );
+            await _context.SaveChangesAsync();
+        }
+
+        private async Task AddToMovieCollection(int movieId, int collectionId)
+        {
+            _context.Add(
+                new MovieCollection()
+                {
+                    CollectionId = collectionId,
+                    MovieId = movieId
+                }
+            );
+            await _context.SaveChangesAsync();
+        }
+
+
+    }
+}
--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -15,5 +15,6 @@ namespace ReelRoster.Data
         }
 
         public DbSet<Collection> Collection { get; set; }
+        public DbSet<Movie> Movie { get; set; }
     }
 }
