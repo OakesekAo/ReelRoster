29ccb21ca421c39946ab238664049ce54bf49c95
OakeseskAo <oakes.a@live.com>
2023-01-23 23:05:17 -0500

    Create ActorsController.cs
    Created the actorsController

---
+++ Controllers/ActorsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReelRoster.Services.Interfaces;
+using System.Threading.Tasks;
+
+namespace ReelRoster.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly IRemoteMovieService _tmdbMovieService;
+        private readonly IDataMappingService _mappingService;
+
+        public ActorsController(IRemoteMovieService tmdbMovieService, IDataMappingService mappingService)
+        {
+            _tmdbMovieService = tmdbMovieService;
+            _mappingService = mappingService;
+        }
+
+
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            var actor = await _tmdbMovieService.ActorDetailAsync(id);
+            actor = _mappingService.MapActorDetail(actor);
+            return View(actor);
+        }
+    }
+}
