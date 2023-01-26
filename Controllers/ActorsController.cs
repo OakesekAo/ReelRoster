6b376ce8e7dc39950933992be4314e2c8c605072
OakeseskAo <oakes.a@live.com>
2023-01-25 22:58:32 -0500

    Updated a few thigns
    Updated a few things

--- Controllers/ActorsController.cs
+++ Controllers/ActorsController.cs
@@ -17,7 +17,7 @@ namespace ReelRoster.Controllers
 
 
 
-        public async Task<IActionResult> Detail(int id)
+        public async Task<IActionResult> Details(int id)
         {
             var actor = await _tmdbMovieService.ActorDetailAsync(id);
             actor = _mappingService.MapActorDetail(actor);
